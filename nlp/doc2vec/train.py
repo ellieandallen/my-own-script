2a4730e2bbbe39a501b9c04b9e7e385356b0ad98
ellieandallen <ellie.yang92@gmail.com>
2017-12-21 20:33:01 +0800

    add multi gpu

--- nlp/doc2vec/train.py
+++ nlp/doc2vec/train.py
@@ -46,3 +46,29 @@ for index, batch in enumerate(batches):
 embeddings = sess.run(model.embeddings)
 np.save('/data/sms_process2/idembeddings.npy', embeddings)
 sess.close()
+
+# config = tf.ConfigProto()
+# config.gpu_options.allow_growth = True
+# config.log_device_placement = True
+# sess = tf.Session(config=config)
+# sess.run(tf.global_variables_initializer())
+# average = collections.deque(maxlen=100)
+
+# for index, batch in enumerate(batches):
+#     num_gpus = 4
+#     four_dict = list(next(enumerate(batches)) for i in range(num_gpus))
+#     # feed_dict = {data: batch[0], target: batch[1]}
+#     # cost, _ = sess.run([model.cost, model.optimize], feed_dict)
+#     for i in range(num_gpus):
+#         with tf.device(tf.DeviceSpec(device_type="GPU", device_index=i)):
+#             with tf.variable_scope(tf.get_variable_scope(), reuse=i > 0):
+#                 feed_dict = {data: four_dict[i][1][0], target: four_dict[i][1][1]}
+#                 cost, _ = sess.run([model.cost, model.optimize], feed_dict)
+#                 average.append(cost)
+#     print('{}: {:5.1f}'.format(index + 1, sum(average) / len(average)))
+#     if index > 100000:
+#         break
+        
+# embeddings = sess.run(model.embeddings)
+# np.save('/data/sms_process2/idembeddings.npy', embeddings)
+# sess.close()
